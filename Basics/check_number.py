6205add53277a4c273e772cb402db06fd46cc12d
Dheeraj raju <dheshal07@gmail.com>
2026-01-14 22:54:14 +0530

    Create check_number.py

---
+++ Basics/check_number.py
@@ -0,0 +1,10 @@
+num=int(input("Enter the number"))
+if num>0:
+    print("The number is positive")
+    if num%2==0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+
+else:
+    print("The number is zero or negative")
