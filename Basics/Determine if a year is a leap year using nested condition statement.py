75cbd67ea9c185137e29c75f86268885a81c7f69
Dheeraj raju <dheshal07@gmail.com>
2026-01-14 23:27:29 +0530

    Update Determine if a year is a leap year using nested condition statement.py

--- Basics/Determine if a year is a leap year using nested condition statement.py
+++ Basics/Determine if a year is a leap year using nested condition statement.py
@@ -1,11 +1,20 @@
+'''
+Note: A year is a leap year if "any one of " the following conditions are satisfied: 
+The year is multiple of 400.
+The year is a multiple of 4 and not a multiple of 100.
+400 == 0
+100 == 1
+4 == 0
+'''
+
 year = int(input("enter the year"))
 if year %400 ==0:
     if year % 100 ==0:
         if year % 4 ==0:
-            print("it is a leap year")
+            print(year,"it is a leap year")
         else:
-            print("it is not a leap year")
+            print(year,"it is not a leap year")
     else:
-        print("it is a leap year")
+        print(year,"it is a leap year")
 else:
-    print("not a leap year")
+    print(year,"not a leap year")
